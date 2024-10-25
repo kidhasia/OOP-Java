0fa260ca25d36409c9a9fb9352706d0e2230bed5
Gaveen Sooriyaarachchi <149564638+kidhasia@users.noreply.github.com>
2024-10-25 11:26:42 +0530

    ArrayList

---
+++ OOP_FINAL/src/OOP_FINAL/Collections.java
@@ -0,0 +1,40 @@
+package OOP_FINAL;
+
+import java.util.ArrayList;
+
+
+public class Collections {
+	public static void main (String args []) {
+		
+		ArrayList <String> fruits =  new ArrayList();
+		fruits.add("Apple");
+		fruits.add("Banana");
+		fruits.add("Grapes");
+		
+		System.out.println(fruits);
+		
+		for(int i =0; i<fruits.size(); i++) {
+			//In array list you cannot use .length only in arrays
+			
+			System.out.println(fruits.get(i));
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
