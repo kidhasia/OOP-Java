513903f9fcb7b8162cb3e5ddb4f1f4343c3136b9
Gaveen Sooriyaarachchi <149564638+kidhasia@users.noreply.github.com>
2024-10-26 18:55:13 +0530

    GejQUe

--- OOP_FINAL/src/OOPQ1P2/InventoryManager.java
+++ OOP_FINAL/src/OOPQ1P2/InventoryManager.java
@@ -1,21 +1,40 @@
 package OOPQ1P2;
 
  class GenericInventory{
-	 
-	 //Creating a bound array
-	 public <T extends Number> void calculateAvaerage(T [] numArray) {
-		 
-		 
-	 }
-	 
-	 public <T extends Number> void calculateMinimum(T [] numArray) {
-		 
-		 
-	 }
 	
+	public <T extends Number> void calculateAverage(T numbArray []) {
+		
+		//Method for calculate the total
+		
+		double total = 0.0;
+		
+		for(T element : numbArray) {
+			total = total+ element.doubleValue();
+		}
+		double average = total / numbArray.length;
+		
+		
+	}
+	
+	public <T extends Number> void calculateMax(T [] numbArray) {
+		
+		double min = numbArray[0].doubleValue();
+		
+		for(T element: numbArray) {
+			if(min>element.doubleValue()) {
+				System.out.println("Minimum number is: "+min);
+			}
+			
+		}
+		
+		
+		
+	}
 }
 
-
 public class InventoryManager{
-	
+	public static void main(String[] args) {
+		
+		
+	}
 }
