ee572e14b14680bede285bf9de751da1b600697a
Gaveen Sooriyaarachchi <149564638+kidhasia@users.noreply.github.com>
2024-10-27 15:22:39 +0530

    Q2 Correct

--- OOP_FINAL/src/OOPQ1P2/InventoryManager.java
+++ OOP_FINAL/src/OOPQ1P2/InventoryManager.java
@@ -1,10 +1,13 @@
 package OOPQ1P2;
 
- class GenericInventory{
+import java.io.InputStream;
+import java.util.Scanner;
+
+class GenericInventory{
 	
-	public <T extends Number> void calculateAverage(T numbArray []) {
+	 //Generic method for calculate the total
+	public <T extends Number> double calculateAverage(T numbArray []) {
 		
-		//Method for calculate the total
 		
 		double total = 0.0;
 		
@@ -13,21 +16,25 @@ package OOPQ1P2;
 		}
 		double average = total / numbArray.length;
 		
+		 return average ;
+		
 		
 	}
 	
-	public <T extends Number> void calculateMax(T [] numbArray) {
+	public <T extends Number> double calculateMin(T numbArray[]) {
 		
 		double min = numbArray[0].doubleValue();
 		
-		for(T element: numbArray) {
-			if(min>element.doubleValue()) {
-				System.out.println("Minimum number is: "+min);
+		for(int i =0; i<numbArray.length; i++){
+			 
+			if(min > numbArray[i].doubleValue()) {
+				min = numbArray[i].doubleValue(); 
+				
 			}
 			
-		}
-		
-		
+		 }
+
+		return min;
 		
 	}
 }
@@ -35,6 +42,35 @@ package OOPQ1P2;
 public class InventoryManager{
 	public static void main(String[] args) {
 		
+		//Creating an instance of GenericInventory
+		GenericInventory giObj1 = new GenericInventory();
+		GenericInventory giObj2 = new GenericInventory();
+		
+		//Creating a scanner object
+		Scanner scanner = new Scanner (System.in);
+		
+		//Create an array
+		
+		System.out.print("Enter the size of the array: ");
+		int arraySize = scanner.nextInt();
+		
+		Double [] numbers = new Double [arraySize];	
+		
+		for(int i=0; i<arraySize; i++) {
+			System.out.print("Enter values: ");
+			numbers[i] = scanner.nextDouble();
+		}
+		
+		double average  = giObj1.calculateAverage(numbers);
+		double min = giObj2.calculateMin(numbers);
+		
+		System.out.print("Average is: "+average+"\n");
+		System.out.print("Minimum is: "+min);
+		
+		
+		
 		
 	}
 }
+
+	
